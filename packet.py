cdcddcf5323ec9afcf61df4b2fc18b24ab477202
raiuny <zouyu@Mac-mini.local>
2024-05-12 23:52:41 +0800

    update

--- packet.py
+++ packet.py
@@ -1,7 +1,8 @@
 
 class Pkt(object):
-    def __init__(self, id, arr_time = -1, ser_time = -1, dep_time = -1):
+    def __init__(self, id, arr_time = -1, ser_time = -1, dep_time = -1, num = -1):
         self.id = id
+        self.num = num
         self.arr_time = arr_time
         self.ser_time = ser_time # 成为HOL包的时间
         self.dep_time = dep_time # 成功传输的时间
--- simulation.py
+++ simulation.py
@@ -6,13 +6,14 @@ from typing import List
 from arrival_model import IntervalGeneratorFactory, ArrivalType
 from packet import Pkt
 import math
-
+from multiprocessing import Pool
 import argparse
 
 def parse_args():
     ap = argparse.ArgumentParser()
-    ap.add_argument("--lam1", type=float, help="lambda1", default=0.002)
-    ap.add_argument("--lam2", type=float, help="lambda2", default=0.002)
+    ap.add_argument("--lam1", type=float, help="arrival rate of mld", default=0.002)
+    ap.add_argument("--lam2", type=float, help="arrival rate of sld", default=0.002)
+    ap.add_argument("--beta", type=float, help="beta", default=0.5)
     ap.add_argument("--nmld", type=int, default=10)
     ap.add_argument("--nsld1", type=int, default=0)
     ap.add_argument("--nsld2", type=int, default=0)
@@ -25,8 +26,10 @@ def parse_args():
     return ap.parse_args()
 
 class Params(object):
-    arrival_rate = 0.002 # per node per slot
+    arrival_rate_mld = 0.004 # per node per slot
+    arrival_rate_sld = 0.002 # per node per slot
     nlink = 2 # 2 links
+    beta = 0.5
     nmld = 10
     nsld1 = 0
     nsld2 = 0
@@ -35,19 +38,33 @@ class Params(object):
     sldW = 16
     sldK = 6
     sim_duration = 1e6
-    queuing_time_link = [[] for _ in range(nlink)]
+    tt = 36
+    tf = 28
+    
+    # MLD
+    queueing_time_link = [[] for _ in range(nlink)]
     access_time_link = [[] for _ in range(nlink)]
     e2e_time_link = [[] for _ in range(nlink)]
     thpt_link = [0 for _ in range(nlink)]
+    suc_link = [0 for _ in range(nlink)]
+    col_link = [0 for _ in range(nlink)]
+    
+    # SLD
+    queueing_time_link_sld = [[] for _ in range(nlink)]
+    access_time_link_sld = [[] for _ in range(nlink)]
+    e2e_time_link_sld = [[] for _ in range(nlink)]
+    thpt_link_sld = [0 for _ in range(nlink)]
+    suc_link_sld = [0 for _ in range(nlink)]
+    col_link_sld = [0 for _ in range(nlink)]
+    
     fin_counter = 0
     pkts_counter = 0
-    tt = 26
-    tf = 21
-    beta = 1
+
 
 class MLD(object):
     def __init__(self, id: int, env: simpy.Environment, links: List[simpy.Resource], arr_type: ArrivalType, lam, init_w, cut_stg, suc_time, col_time, beta):
         self.id = id
+        self.type = (beta == 0) or (beta == 1)
         self.env = env      
         # self.action = start_delayed(env, self.run(), )
         self.beta = beta
@@ -82,7 +99,7 @@ class MLD(object):
             itv = self.arrival_interval()
             yield self.env.timeout(itv)
             self.pkt_num += 1
-            pkt = Pkt(self.id, self.env.now,)
+            pkt = Pkt(self.id, self.env.now,num=self.pkt_num)
             Params.pkts_counter += 1
             self.allocating(pkt) 
             
@@ -99,38 +116,53 @@ class MLD(object):
                         
     def try_connecting(self, linkid):
         while True:
-            if self.links[linkid].count == 0:
-                if len(self.pkts_on_link[linkid]) > 0:
-                    assert self.pkts_on_link[linkid][0].ser_time!=-1, "HOL包无开始服务的时间"
-                    if self.bocs[linkid] == 0:
-                        with self.links[linkid].request() as req:
-                            if not req.triggered:
-                                yield self.env.timeout(self.col_time)
-                                self.reset_bow(linkid, 1)
-                                self.reset_boc(linkid)
+            if len(self.pkts_on_link[linkid]) > 0:
+                assert self.pkts_on_link[linkid][0].ser_time!=-1, "HOL包无开始服务的时间"
+                if self.bocs[linkid] == 0:
+                    with self.links[linkid].request() as req:
+                        if not req.triggered:
+                            yield self.env.timeout(self.col_time)
+                            self.reset_bow(linkid, 1)
+                            self.reset_boc(linkid)
+                            if self.type: # SLD
+                                Params.col_link_sld[linkid] += 1
                             else:
-                                yield self.env.timeout(self.suc_time)
-                                self.reset_bow(linkid, 0)
-                                self.reset_boc(linkid)
-                                # print(self.pkts_on_link)
-                                pkt = self.pkts_on_link[linkid].pop(0)
-                                pkt.dep_time = self.env.now
-                                Params.queuing_time_link[linkid].append(pkt.ser_time - pkt.arr_time) 
+                                # if self.id == 1:
+                                #     print("collide: ", self.env.now, self.pkts_on_link[linkid][0].num)
+                                Params.col_link[linkid] += 1
+                        else:
+                            yield self.env.timeout(self.suc_time)
+                            self.reset_bow(linkid, 0)
+                            self.reset_boc(linkid)
+                            # print(self.pkts_on_link)
+                            pkt = self.pkts_on_link[linkid].pop(0)
+                            pkt.dep_time = self.env.now
+                            if self.type: # SLD
+                                Params.queueing_time_link_sld[linkid].append(pkt.ser_time - pkt.arr_time) 
+                                Params.access_time_link_sld[linkid].append(pkt.dep_time - pkt.ser_time)
+                                Params.e2e_time_link_sld[linkid].append(pkt.dep_time - pkt.arr_time)
+                                Params.fin_counter += 1
+                                Params.thpt_link_sld[linkid] += 1
+                                Params.suc_link[linkid] += 1
+                            else: # MLD
+                                Params.queueing_time_link[linkid].append(pkt.ser_time - pkt.arr_time) 
                                 Params.access_time_link[linkid].append(pkt.dep_time - pkt.ser_time)
                                 Params.e2e_time_link[linkid].append(pkt.dep_time - pkt.arr_time)
                                 Params.fin_counter += 1
                                 Params.thpt_link[linkid] += 1
-                                if len(self.pkts_on_link[linkid]) > 0 and self.pkts_on_link[linkid][0].ser_time == -1:
-                                    self.pkts_on_link[linkid][0].ser_time = self.env.now
-                    else:
-                        yield self.env.timeout(1)
-                        self.bocs[linkid] -= 1
+                                Params.suc_link[linkid] += 1
+                            if len(self.pkts_on_link[linkid]) > 0 and self.pkts_on_link[linkid][0].ser_time == -1:
+                                self.pkts_on_link[linkid][0].ser_time = self.env.now
                 else:
+                    if self.links[linkid].count == 0:
+                        self.bocs[linkid] -= 1
                     yield self.env.timeout(1)
-                    self.bocs[linkid] = self.bocs[linkid] - 1 if self.bocs[linkid] > 0 else 0
+                    
             else:
+                if self.links[linkid].count == 0:
+                    self.bocs[linkid] = self.bocs[linkid] - 1 if self.bocs[linkid] > 0 else 0
                 yield self.env.timeout(1)
-            
+        
                     
     
     def reset_bow(self, link_idx, flag = 0):
@@ -142,7 +174,7 @@ class MLD(object):
     def reset_boc(self, link_idx):
         self.bocs[link_idx] = self.boc_rngs[link_idx].randint(0, self.bows[link_idx])
     
-        
+    
 class System(object):
     def __init__(self):
         self.env = simpy.Environment()
@@ -153,12 +185,12 @@ class System(object):
         for i in range(Params.nlink):
             self.links.append(simpy.Resource(self.env, capacity=1)) # 一般资源 先进先出
         for i in range(Params.nmld):
-            self.mlds.append(MLD(i, self.env, self.links, ArrivalType.BERNOULLI, Params.arrival_rate, Params.mldW, Params.mldK, Params.tt, Params.tf, beta = Params.beta))
+            self.mlds.append(MLD(i, self.env, self.links, ArrivalType.BERNOULLI, Params.arrival_rate_mld, Params.mldW, Params.mldK, Params.tt, Params.tf, beta = Params.beta))
         # SLD
         for i in range(Params.nsld1):
-            self.mlds.append(MLD(i, self.env, self.links, ArrivalType.BERNOULLI, Params.arrival_rate, Params.sldW, Params.sldK, Params.tt, Params.tf, beta = 1))
+            self.mlds.append(MLD(i, self.env, self.links, ArrivalType.BERNOULLI, Params.arrival_rate_sld, Params.sldW, Params.sldK, Params.tt, Params.tf, beta = 1))
         for i in range(Params.nsld2):
-            self.mlds.append(MLD(i, self.env, self.links, ArrivalType.BERNOULLI, Params.arrival_rate, Params.sldW, Params.sldK, Params.tt, Params.tf, beta = 0))
+            self.mlds.append(MLD(i, self.env, self.links, ArrivalType.BERNOULLI, Params.arrival_rate_sld, Params.sldW, Params.sldK, Params.tt, Params.tf, beta = 0))
         # 优先资源: PriorityResource 一般资源: Resource (FCFS)
             
     def run(self):
@@ -166,23 +198,76 @@ class System(object):
         for mld in self.mlds:
             mld.run()
         self.env.run(until=Params.sim_duration)
-        return np.mean(Params.queuing_time_link[0]), np.mean(Params.access_time_link[0]), np.mean(Params.e2e_time_link[0])
     
     def step_process(self):
         yield self.env.timeout(5)
-        print(self.env.now, np.mean(Params.queuing_time_link[0]), np.mean(Params.access_time_link[0]), np.mean(Params.e2e_time_link[0]), np.mean(Params.e2e_time_link[1]))
+        print(self.env.now, np.mean(Params.queueing_time_link[0]), np.mean(Params.access_time_link[0]), np.mean(Params.e2e_time_link[0]), np.mean(Params.e2e_time_link[1]))
 
+    def print_result(self):
+        print("Throughput:\n(MLD)")
+        for i in range(Params.nlink):
+            print(Params.thpt_link[i] / Params.sim_duration, end="\t")
+        print("\n(SLD)")
+        for i in range(Params.nlink):
+            print(Params.thpt_link_sld[i] / Params.sim_duration, end="\t")
+        print()
+        print("\nDelay:\n(MLD)")
+        print("Access Delay:")
+        for i in range(Params.nlink):
+            if len(Params.access_time_link[i]) == 0:
+                print(np.nan, end="\t")
+            else:
+                print(np.mean(Params.access_time_link[i]), end="\t")
+        print("\nQueueing Delay:")
+        for i in range(Params.nlink):
+            if len(Params.queueing_time_link[i]) == 0:
+                print(np.nan, end="\t")
+            else:
+                print(np.mean(Params.queueing_time_link[i]), end="\t")
+        print()
+        print("\n(SLD)")
+        print("Access Delay:")
+        for i in range(Params.nlink):
+            if len(Params.access_time_link_sld[i]) == 0:
+                print(np.nan, end="\t")
+            else:
+                print(np.mean(Params.access_time_link_sld[i]), end="\t")
+        print("\nQueueing Delay:")
+        for i in range(Params.nlink):
+            if len(Params.queueing_time_link_sld[i]) == 0:
+                print(np.nan, end="\t")
+            else:
+                print(np.mean(Params.queueing_time_link_sld[i]), end="\t")
+        print()
+        print("\nsuccess transmit prob:")
+        print("(MLD)")
+        for i in range(Params.nlink):
+            if (Params.suc_link[i] + Params.col_link[i]) == 0:
+                print(np.nan, end = "\t")
+            else:
+                print(Params.suc_link[i] / (Params.suc_link[i] + Params.col_link[i]), end="\t")
+        print("\n(SLD)")
+        for i in range(Params.nlink):
+            if (Params.suc_link_sld[i] + Params.col_link_sld[i]) == 0:
+                print(np.nan, end = "\t")
+            else:
+                print(Params.suc_link_sld[i] / (Params.suc_link_sld[i] + Params.col_link_sld[i]), end="\t")
+        print()
+        print(f"success transmit ratio: {Params.fin_counter / Params.pkts_counter :.4f}") 
 if __name__ == "__main__":
+    begin = time()
     args = parse_args()
     print(args)
+    Params.arrival_rate_mld = args.lam1
+    Params.arrival_rate_sld = args.lam2
+    Params.nmld = args.nmld
+    Params.nsld1 = args.nsld1
+    Params.nsld2 = args.nsld2
+    Params.beta = args.beta
+    Params.mldW = args.mldW1
+    Params.mldK = args.mldK1
     sys = System()
-    qd, ad, ed = sys.run()
-    run_time = Params.sim_duration
-    print("result", qd, ad, ed)
-    served_num, total_num = Params.fin_counter, Params.pkts_counter
-    total = 0
-    for mld in sys.mlds:
-        total += mld.pkt_num
-    print(served_num, total_num, total)
-    print("throughput on link1: ", Params.thpt_link[0]/Params.sim_duration)
-    print(Params.arrival_rate * Params.sim_duration)
+    sys.run()
+    end = time()
+    sys.print_result()
+    print(f"Total time: {end-begin:.4f} sec.")
